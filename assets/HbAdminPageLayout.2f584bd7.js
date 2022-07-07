a826933ffa8e15002cf20cf75b6b9e1a46cee094
pumelotea <30773737+pumelotea@users.noreply.github.com>
2022-07-07 03:12:03 +0000

    Deploying to gh-pages from  @ baa9fe3635e3dbcba8d5e0cc555a00eb0097a1d8 🚀

---
+++ assets/HbAdminPageLayout.2f584bd7.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./index.aaf46fa6.js";import{f as t,g as _,Z as o}from"./vendor.9a5280d9.js";const r={},n={class:"hb-admin-page-layout"};function s(e,d){return t(),_("div",n,[o(e.$slots,"default",{},void 0,!0)])}var u=a(r,[["render",s],["__scopeId","data-v-23b46d98"]]);export{u as H};
